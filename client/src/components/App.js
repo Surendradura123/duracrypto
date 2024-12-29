0724aecd39dacf8c0ff1d9b5b9c62c3508d7f4fe
Surendradura123 <suren.dura@yahoo.com>
2024-12-29 13:29:09 +0000

    frontend part added

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Blocks from './Blocks';
 
 class App extends Component {
 
@@ -18,6 +19,8 @@ class App extends Component {
             <div>Welcome to the blockchain...</div>
             <div>Address: {address}</div>
             <div>Balance: {balance}</div>
+            <br />
+            <Blocks />
         </div>
         );
     }
